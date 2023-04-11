db20acb502877afb4a50626e13853ec4f7586901
RomaSamkov <Romansamkov.rs@gmail.com>
2023-04-11 21:55:48 +0300

    Create database.ts

---
+++ database.ts
@@ -0,0 +1,37 @@
+interface DataBase {
+  get(id: string): string;
+  set(id: string, value: string): void;
+}
+
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storedState: string): void;
+}
+
+class InMemoryDataBase implements DataBase {
+  protected db: Record<string, string> = {};
+  get(id: string): string {
+    return this.db[id];
+  }
+  set(id: string, value: string): void {
+    this.db[id] = value;
+  }
+}
+
+class PersistentMemoryDB extends InMemoryDataBase implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db);
+  }
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState);
+  }
+}
+
+const myDB = new PersistentMemoryDB();
+myDB.set("TS", "JavaScript");
+console.log(myDB.get("TS"));
+const saved = myDB.saveToString();
+
+const myDB2 = new PersistentMemoryDB();
+myDB2.restoreFromString(saved);
+console.log(myDB2.get("TS"));
