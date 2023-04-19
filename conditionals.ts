9e7272c6aa742c9a47fd050bc15199c7dc9d013f
RomaSamkov <Romansamkov.rs@gmail.com>
2023-04-19 20:58:41 +0300

    Create conditionals.ts

---
+++ conditionals.ts
@@ -0,0 +1,34 @@
+import fetch from "node-fetch";
+
+interface PokemonResults {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+}
+type FetchPokemonResults<T> = T extends undefined
+  ? Promise<PokemonResults>
+  : void;
+
+function fetchPokemon<
+  T extends undefined | (<PokemonResults>(data: PokemonResults) => void)
+>(url: string, cb?: T): FetchPokemonResults<T> {
+  if (cb) {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(cb);
+    return undefined as FetchPokemonResults<T>;
+  } else {
+    return fetch(url).then((resp) => resp.json()) as FetchPokemonResults<T>;
+  }
+}
+
+fetchPokemon(
+  "https://pokeapi.co/api/v2/pokemon?limit=10",
+  <PokemonResults>(data) => {
+    data.results.forEach(({ name }) => console.log(name));
+  }
+);
